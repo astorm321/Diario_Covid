7c657a5e0f7f1ce98da1e032cb690c144b089876
Vitor Pereira <vitor123@live.com.pt>
2020-04-22 19:04:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/diario_covid/DadosPaciente.java
@@ -0,0 +1,14 @@
+package com.example.diario_covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DadosPaciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados_paciente);
+    }
+}
---
+++ app/src/main/java/com/example/diario_covid/Sintomas_Paciente.java
@@ -0,0 +1,14 @@
+package com.example.diario_covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sintomas_Paciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sintomas__paciente);
+    }
+}
